5ec9036fb328feac02519b65dc388c81d5af884a
bizoton19 <bizoton19@gmail.com>
2016-03-18 09:16:39 -0400

    batch notifications up

--- Status/SignalRStatusNotification/StateMonitor.cs
+++ Status/SignalRStatusNotification/StateMonitor.cs
@@ -9,6 +9,7 @@ using System.Web.Hosting;
 using SignalRStatusNotification;
 using Status;
 using System.Net.Sockets;
+using System.Net;
 
 namespace SignalRStatusNotification
 {
@@ -16,50 +17,138 @@ namespace SignalRStatusNotification
     {
         private IHubContext _hubs;
         private int _interval;
-        private List<string> urls;
-        public StateMonitor(int updateInterval, List<string> resourceUris)
+        private List<Resource> urls;
+        private HashSet<State> _states = new HashSet<State>();
+        private string _notificationRecipients;
+
+        public StateMonitor(int updateInterval, List<Resource> resourceUris)
         {
             _hubs = GlobalHost.ConnectionManager.GetHubContext<StatusNotificationHub>();
            this._interval = updateInterval;
-           this.urls = resourceUris;
+            urls = resourceUris;
         }
         public async Task Init()
         {
-
-            List<Resource> resources = new List<Resource>();
+            _notificationRecipients = System.Configuration.ConfigurationManager.AppSettings["To"].ToString();
+            await Task.Run(new Action(BeginPolling));
+        
+                if (_states.Any())
+                {
+                    StateLogger.SendAlertNotification(_states, _notificationRecipients);
+                }
             
-            urls.ForEach(s => resources.Add(new HttpResource(new Uri(s)))); 
-           
+        }
+
+        private void BeginPolling()
+        {
+
+
             while (ResourcePoller.IsNetworkAvailable())
             {
-              foreach(var r in resources)
-              {
-                  await BeginStateMonitor(r);
-              }
+                foreach (var resource in urls)
+                {
+                    try
+                    {
+
+                        if (resource.GetType() == typeof(WindowsService))
+                            MonitorWindowsService(resource);
+                        if (resource.GetType() == typeof(Server))
+                            MonitorServer(resource);
+                        if (resource.GetType() == typeof(HttpResource))
+                            MonitorHttpResource(resource);
+                    }
+                    catch (Exception ex)
+                    {
+                        IList<State> st = new List<State>();
+                        st.Add(new State() { Status = ex.Message + "-" + ex.StackTrace });
+                        StateLogger.SendAlertNotification(st, _notificationRecipients);
+                    }
+
+                }
+                if (_states.Any()) { 
+                StateLogger.SendAlertNotification(_states, _notificationRecipients);
+                }
+
 
             }
         }
 
-        private async Task BeginStateMonitor(Resource resource)
+        private void MonitorWindowsService(Resource resource)
         {
             ResourcePoller poller = new ResourcePoller(resource, this._interval);
+            Task<State> pollresult = poller.PollWindowsServiceAsync();
 
-            System.Threading.Tasks.Task<State> result = poller.PollAsync();
-            // _hubs.Clients.All.broadcast"Polling resource with internal id of : {0} in {1} ms intervals...", resources.IndexOf(resource), poller.PollingInterval);
+            State state = pollresult.Result;
+            Console.WriteLine("Result: " + pollresult.Result.Url + " " + pollresult.Result.Status);
+            if (pollresult.Result.Status != "Running" && !_states.Contains(pollresult.Result))
+            {
+                _states.Add(pollresult.Result);
+            }
+            else
+            {
+                RemoveStateFromNotificationList(pollresult);
+            }
+            
+        }
+        private void MonitorServer(Resource resource)
+        {
+            ResourcePoller poller = new ResourcePoller(resource, this._interval);
+            Task<State> pollresult = poller.PollServerAsync();
 
-            if (result.IsCompleted)
-                Console.WriteLine("Result: " + result.Result.Url + " " + result.Result.Status);
-                try
-                {
-                    await _hubs.Clients.All.broadcastStatus(result.Result.Url, result.Result.Status, DateTime.UtcNow.ToString());
-                }
-                catch (SocketException e)
-                {
-                    // Console.WriteLine("An exception occurred while processing the request: {0}", e.Message);
-                }
+            State state = pollresult.Result;
+            Console.WriteLine("Result: " + pollresult.Result.Url + " " + pollresult.Result.Status);
+
+            if (pollresult.Result.Status != "Success" && !_states.Contains(pollresult.Result))
+            {
+                _states.Add(pollresult.Result);
+            }
+            else
+            {
+                RemoveStateFromNotificationList(pollresult);
+            }
+
+
+        }
+        private void MonitorHttpResource(Resource resource)
+        {
+            ResourcePoller poller = new ResourcePoller(resource, this._interval);
+            Task<State> pollresult = poller.PollHttpAsync();
+
+
+            State state = pollresult.Result;
+            Console.WriteLine("Result: " + pollresult.Result.Url + " " + pollresult.Result.Status);
+
+            if (pollresult.Result.Status != HttpStatusCode.OK.ToString() && !_states.Contains(pollresult.Result))
+            {
+
+                _states.Add(pollresult.Result);
+
+            }
+            else
+            {
+                RemoveStateFromNotificationList(pollresult);
+            }
         }
 
-        
-        
+        private void RemoveStateFromNotificationList(Task<State> pollresult)
+        {
+            if (_states.Contains(pollresult.Result))
+            {
+                _states.Remove(pollresult.Result);
+            }
+        }
+
+        // try
+        // {
+        // await _hubs.Clients.All.broadcastStatus(result.Result.Url, result.Result.Status, DateTime.UtcNow.ToString());
+        // }
+        // catch (SocketException e)
+        // {
+        // Console.WriteLine("An exception occurred while processing the request: {0}", e.Message);
+        // }
+
+
+
+
     }
 }
