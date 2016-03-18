1d759e1191354cd39a8767b96853f21cdc5336ab
bizoton19 <bizoton19@gmail.com>
2016-03-18 09:17:04 -0400

    refactored to use resouce factory

--- Status/SignalRStatusNotification/Startup.cs
+++ Status/SignalRStatusNotification/Startup.cs
@@ -18,13 +18,17 @@ namespace SignalRStatusNotification
         public void Configuration(IAppBuilder app)
         {
             // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
-            
+            List<Resource> resources = new List<Resource>();
             var hubConfiguration = new HubConfiguration();
             hubConfiguration.EnableDetailedErrors = true;
-
             app.MapSignalR(hubConfiguration);
-            var urls = System.Configuration.ConfigurationManager.AppSettings["resources"].Split(',').ToList();
-            StateMonitor monitor = new StateMonitor(20000, urls);
+            System.Configuration.ConfigurationManager
+                .AppSettings["resources"]
+                .Split(',')
+                .ToList()
+                .ForEach(s => resources.Add(new ResourceFactory().GetResource(s)));
+
+            StateMonitor monitor = new StateMonitor(20000, resources);
             Task.Factory.StartNew(async() => await monitor.Init());
 
             
